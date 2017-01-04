4742acafe04fd0a214a06b480f1b2d0be10a22f0
Rob Huehn <rhuehn@gmail.com>
2017-01-04 14:54:05 -0800

    remove my local devices

--- start.js
+++ start.js
@@ -1,50 +1,21 @@
 'use strict'
 const
     bus = require('@theatersoft/bus').default,
-    service = {
+    options = {
         module: '@theatersoft/x10',
         export: 'X10',
         name: 'X10',
         config: {
             vid: 0x0bc7, pid: 0x0001, devices: [
                 {
-                    "name": "Studio",
+                    "name": "Test",
                     "id": "A1"
-                },
-                {
-                    "name": "Front",
-                    "id": "A2"
-                },
-                {
-                    "name": "Garage",
-                    "id": "A3"
-                },
-                {
-                    "name": "Path",
-                    "id": "A4"
-                },
-                {
-                    "name": "Deck",
-                    "id": "A5"
-                },
-                {
-                    "name": "Theater",
-                    "id": "A6",
-                    "dim": "true"
-                },
-                {
-                    "name": "Garage",
-                    "id": "A7",
-                    "device": "PR511"
-                },
-                {
-                    "name": "Office motion",
-                    "id": "A8",
-                    "device": "MS14A"
                 }
             ]
         }
-    }
+    },
+    service = new (require(options.module)[options.export])()
+
 
 bus.start().then(() =>
-    new (require(service.module)[service.export])().start(service))
+    service.start(options))
