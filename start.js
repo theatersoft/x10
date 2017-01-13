eedf0540b29e840b049b458de45d8f48e2e0acee
Rob Huehn <rhuehn@gmail.com>
2017-01-12 20:29:02 -0800

    load start script options from Config service

--- start.js
+++ start.js
@@ -1,21 +1,13 @@
 'use strict'
-const
-    bus = require('@theatersoft/bus').default,
-    options = {
-        module: '@theatersoft/x10',
-        export: 'X10',
-        name: 'X10',
-        config: {
-            vid: 0x0bc7, pid: 0x0001, devices: [
-                {
-                    "name": "Test",
-                    "id": "A1"
-                }
-            ]
-        }
-    },
-    service = new (require(options.module)[options.export])()
-
-
-bus.start().then(() =>
-    service.start(options))
+require('@theatersoft/bus').default.start()
+    .then(bus =>
+        bus.proxy('Config').get()
+            .then(config => {
+                const
+                    hostname = require('os').hostname(),
+                    options = config.Hosts
+                        .find(({Name}) => Name === hostname).services
+                        .find(({name}) => name === 'X10'),
+                    service = new (require(options.module)[options.export])()
+                service.start(options)
+            }))
