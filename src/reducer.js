635b406b89346e7ea76034d160ce55f42d7df3ee
Rob Huehn <rhuehn@gmail.com>
2017-01-25 20:28:19 -0800

    reorder device props

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,7 @@
 import {ON, on, OFF, off} from './actions'
 import {INIT_DEVICES} from './actions'
 
+const reorder = arr => arr.map(({name, value, type, id}) => ({name, value, type, id}))
 const index = arr => arr.reduce((o, e) => (o[e.id] = e, o), {})
 
 export default function reducer (state, {type, devices, id}) {
@@ -8,7 +9,7 @@ export default function reducer (state, {type, devices, id}) {
     case INIT_DEVICES:
         return {
             ...state,
-            devices: index(devices)
+            devices: index(reorder(devices))
         }
     case ON:
     case OFF:
