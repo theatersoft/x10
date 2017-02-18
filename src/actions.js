7380a399bb7cece0e43ffd51d27cec2620cb3f69
Rob Huehn <rhuehn@gmail.com>
2017-02-18 13:47:44 -0800

    import switchActions from device

--- src/actions.js
+++ src/actions.js
@@ -5,16 +5,8 @@ export const
     INIT_DEVICES = 'INIT_DEVICES',
     initDevices = devices => ({type: INIT_DEVICES, devices})
 
-export const
-    ON = 'ON',
-    OFF = 'OFF',
-    BRIGHT = 'BRIGHT',
-    DIM = 'DIM'
-export const
-    on = id => ({type: ON, id}),
-    off = id => ({type: OFF, id}),
-    bright = (id, n) => ({type: BRIGHT, id, n}),
-    dim = (id, n) => ({type: DIM, id, n})
+import {switchActions} from '@theatersoft/device'
+export const {ON, OFF, on, off} = switchActions
 
 export const
     RX = 'RX',
