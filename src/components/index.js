e534c6d4c8a9554794fb9dbaf9366c454ef67030
Rob Huehn <rhuehn@gmail.com>
2017-07-24 07:48:03 -0700

    add service component

---
+++ src/components/index.js
@@ -0,0 +1,38 @@
+import {h, Component} from 'preact'
+import {ListItem, Switch} from '@theatersoft/components'
+import {proxy} from '@theatersoft/bus'
+import {connect} from './redux'
+
+const Settings = proxy('Settings')
+export const settingsAction = state => () => Settings.setState(state)
+
+const
+    mapState = p => p,
+    mapDispatch = dispatch => ({
+        dispatch: {
+            settings: state => dispatch(settingsAction(state))
+        }
+    })
+
+export const ServiceSettings = (ComposedComponent, props) => connect(mapState, mapDispatch)(class ServiceSettings extends Component {
+    onClick = e => {
+        const
+            [, service, id] = /^([^\.]+)\.([^]+)$/.exec(e.currentTarget.dataset.id),
+            value = this.props[service][id]
+        this.props.dispatch[service]({[id]: !value})
+    }
+
+    onChange = (value, e) => this.onClick(e)
+
+    render ({settings}) {
+        const
+            item = (label, value, id) =>
+                <ListItem label={label}>
+                    <Switch checked={value} data-id={id} onChange={this.onChange}/>
+                </ListItem>
+        return (
+            <ComposedComponent {...props}>
+            </ComposedComponent>
+        )
+    }
+})
---
+++ src/components/redux.js
@@ -0,0 +1,3 @@
+import api from 'preact-redux'
+export const {Provider} = api
+export const {connect} = api
