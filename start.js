92b0c053504bccb1598eb443a8a7a5b5f97991b9
Rob Huehn <rhuehn@gmail.com>
2017-12-18 11:19:23 -0800

    convert to startLocalService

--- start.js
+++ start.js
@@ -1,17 +1,1 @@
-'use strict'
-require('@theatersoft/bus').setTime(true)
-require('@theatersoft/bus').default.start()
-    .then(bus =>
-        bus.proxy('Config').get()
-            .then(config => {
-                const
-                    hostname = require('os').hostname(),
-                    options = config.hosts
-                        .find(({name}) => name === hostname).services
-                        .find(({name}) => name === 'X10'),
-                    service = new (require(options.module)[options.export])()
-                Object.assign(options.config, config.configs[options.name])
-                service.start(options)
-                process.on('SIGINT', () =>
-                    service.stop().then(() => process.exit()))
-            }))
+require('@theatersoft/server/lib').startLocalService('X10')
